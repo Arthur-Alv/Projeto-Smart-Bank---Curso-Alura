1685a0ec38744fd3f008722f417f977aaaea0755
Arthur Magalhães <arthuralvesma10@hotmail.com>
2022-05-09 11:35:22 -0300

    Add files via upload

---
+++ build/precache-manifest.d7888477e0039e8c46de8141d6e9e66d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "313f82f690b14fafb9c3cbfa6d5d1865",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70deda4ad9d0ecfe62b1",
+    "url": "/static/css/main.9e86ca62.chunk.css"
+  },
+  {
+    "revision": "0af4e56daa9094802bee",
+    "url": "/static/js/2.cbb83cde.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cbb83cde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70deda4ad9d0ecfe62b1",
+    "url": "/static/js/main.046905c8.chunk.js"
+  },
+  {
+    "revision": "927d5e5b2fc5e8a904b3",
+    "url": "/static/js/runtime-main.e6b65449.js"
+  },
+  {
+    "revision": "f3a2bff49642ca68b3841fde422295cb",
+    "url": "/static/media/bank_logo.f3a2bff4.svg"
+  },
+  {
+    "revision": "b97c300bed11302a8687c16e07aa89e5",
+    "url": "/static/media/dinheiro.b97c300b.svg"
+  },
+  {
+    "revision": "acd41ceb3366a71c61b7ed3d5df83682",
+    "url": "/static/media/olho.acd41ceb.svg"
+  },
+  {
+    "revision": "134d853a95b2b30676da2c63cda2eeaf",
+    "url": "/static/media/privado.134d853a.svg"
+  }
+]);
